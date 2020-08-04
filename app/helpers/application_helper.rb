7a0b5c6f95a69cd306d38262e01c96588cd4b51f
gwiedeman <gregory.wiedeman1@gmail.com>
2020-08-04 14:38:20 -0400

    added decades to home page, fixed sorting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+	require 'net/http'
 
 	def subject_options
 		config=YAML.load_file(Rails.root.join("config/subjects.yml"))
@@ -7,6 +8,15 @@ module ApplicationHelper
 			subject
 			url
 		end
+	end
+
+	def decade_counter(search_path, decade_start, decade_end)
+
+		url = URI('https://archives.albany.edu/history/catalog.json' + search_path)
+		response = Net::HTTP.get(url)
+		res = JSON.parse(response)
+		
+		return res["meta"]["pages"]["total_count"]
 	end
 
 end
