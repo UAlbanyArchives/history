716e75e78f8a2a96495cce49b949ba0926dd2250
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2021-01-07 15:43:50 -0500

    fixed home columns, search box focus, and added about card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,9 +16,21 @@ module ApplicationHelper
 
 	def decade_counter(search_path, decade_start, decade_end)
 
-		url = URI('https://archives.albany.edu/history/catalog.json' + search_path)
-		response = Net::HTTP.get(url)
-		res = JSON.parse(response)
+		uri = URI('https://archives.albany.edu/history/catalog.json' + search_path)
+		#raise uri.path
+
+		req = Net::HTTP::Get.new(uri)
+
+		res = Net::HTTP.start(
+			uri.host, uri.port, 
+			:use_ssl => uri.scheme == 'https', 
+			:verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|
+			https.request(req)
+		end
+
+		#response = Net::HTTP.get(url)
+		res = JSON.parse(res.body)
+		puts res["meta"]["pages"]["total_count"]
 		
 		return res["meta"]["pages"]["total_count"]
 	end
