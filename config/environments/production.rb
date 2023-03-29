707d442e3b7f5139fc404da978ea0d05831065ac
Greg Wiedeman <gregory.wiedeman1@gmail.com>
2023-03-29 13:54:11 -0400

    fixed assets config and cleaned and recompiled assets:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   config.assets.prefix = '/history'
-  config.relative_url_root = '/books'
+  config.relative_url_root = '/history'
   config.public_file_server.enabled = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
